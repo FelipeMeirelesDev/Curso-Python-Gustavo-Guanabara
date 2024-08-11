96bc351e55852786c6e1bf3fc0b0d375886bc511
Felipe Meireles <145486045+FelipeMeirelesDev@users.noreply.github.com>
2024-08-11 16:24:24 -0300

    5 Primeiros exercícios

---
+++ ex003.py
@@ -0,0 +1,4 @@
+#Crie um programa que receba dois números e a soma deles
+num1 = int(input("Digite o número 01:"))
+num2 = int(input("Digite o número 02:"))
+print("O resultado da soma dos dois números:{}".format(num1+num2))
---
+++ ex005.py
@@ -0,0 +1,3 @@
+#leia um número e mostre se sucessor e antecessor
+num = int(input("Digite um número:"))
+print("O seu antecessor é {} e sucessor é {}".format((num-1),(num+1)))
