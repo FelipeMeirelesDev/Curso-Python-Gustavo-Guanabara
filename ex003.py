530dd6ecedeed5f241f0c470673e66c471ca4fe6
Felipe Meireles <145486045+FelipeMeirelesDev@users.noreply.github.com>
2024-08-13 06:34:52 -0300

    Delete ex003.py

--- ex003.py
+++
@@ -1,4 +0,0 @@
-#Crie um programa que receba dois números e a soma deles
-num1 = int(input("Digite o número 01:"))
-num2 = int(input("Digite o número 02:"))
-print("O resultado da soma dos dois números:{}".format(num1+num2))
